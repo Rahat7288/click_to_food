8024e3e47f70cf0c176ec80e11f146464a483a79
Rahat Islam Akash <rahatislamakash@gmail.com>
2024-12-17 02:29:38 +0600

    app color and app_url class added

--- lib/core/constants/app_themes/app_colors.dart
+++ lib/core/constants/app_themes/app_colors.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
-  static const Color backGroundColor = Color(0XFFF7F7F7);
+  static const Color backGroundColor = Color(0XFFFFFFFF);
   static const Color activeProgressColor = Color(0XFF06D001);
   static const Color inactiveProgressColor = Color(0XFF89A8B2);
-  static const Color primaryButtonColor = Color(0XFF563A9C);
+  static const Color primaryButtonColor = Color(0XFFA1045A);
   static const Color secondaryButtonColor = Color(0XFF8B5DFF);
   static const Color redButtonColor = Color(0XFFFF6464);
   static const Color titleTextColor = Color(0XFF021526);
---
+++ lib/core/resources/app_urls.dart
@@ -0,0 +1,11 @@
+class AppUrls {
+  static const String baseUrl = "https://demo-api.devdata.top/api";
+
+  static const String checkUserExists =
+      "$baseUrl/Administrator/CheckUserExists";
+  static const String login = "$baseUrl/Administrator/Login";
+  //for new user
+  static const String sendOTPEmail = "$baseUrl/Administrator/SendOTPToEmail";
+  static const String checkOTPFromEmail =
+      "$baseUrl/Administrator/CheckOTPFromEmail";
+}
