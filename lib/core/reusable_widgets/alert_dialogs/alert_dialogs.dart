31a9d4ca316b8785beef03478495d898b1fe059b
Rahat Islam Akash <rahatislamakash@gmail.com>
2024-12-18 01:19:04 +0600

    custom alert dialog widget added

---
+++ lib/core/reusable_widgets/alert_dialogs/alert_dialogs.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+///reusable alertDialog
+///required parameter is child widget (alertDialog body)
+
+class AlertDialogs extends StatelessWidget {
+  const AlertDialogs({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final double mHeight = MediaQuery.of(context).size.height;
+    final double mWidth = MediaQuery.of(context).size.width;
+    return AlertDialog(
+      insetPadding: const EdgeInsets.all(10),
+      content: SizedBox(
+        height: mHeight * .4,
+        width: mWidth,
+        child: child,
+      ),
+    );
+  }
+}
